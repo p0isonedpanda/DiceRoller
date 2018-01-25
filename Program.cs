d98bbd8603c45f2fb54fe35c62862e88b6d2684e
Daniel Coady <daniel.coady@outlook.com.au>
2018-01-25 12:17:22 +1100

    Added capability to use modifiers on rolls

--- Program.cs
+++ Program.cs
@@ -8,30 +8,61 @@ namespace DiceRoller
         {
             Console.WriteLine("Please enter your dice roll:");
             string[] usrInput = Console.ReadLine().Split("d");
-            if (usrInput.Length != 2)
+            if (usrInput.Length < 2 || usrInput.Length > 3)
             {
                 Console.WriteLine("Please use valid formatting");
                 return;
             }
             
-            int rolled, faces;
-            try
+            int rolled, faces, modifier;
+            if (usrInput[1].Split("+").Length == 2)
             {
-                rolled = Convert.ToInt32(usrInput[0]);
-                faces = Convert.ToInt32(usrInput[1]);
+                try
+                {
+                    rolled = Convert.ToInt32(usrInput[0]);
+                    faces = Convert.ToInt32(usrInput[1].Split("+")[0]);
+                    modifier = Convert.ToInt32(usrInput[1].Split("+")[1]);
+                }
+                catch
+                {
+                    Console.WriteLine("Please use valid formatting");
+                    return;
+                }
             }
-            catch
+            else if (usrInput[1].Split("-").Length == 2)
             {
-                Console.WriteLine("Please use valid formatting");
-                return;
+                try
+                {
+                    rolled = Convert.ToInt32(usrInput[0]);
+                    faces = Convert.ToInt32(usrInput[1].Split("-")[0]);
+                    modifier = Convert.ToInt32(usrInput[1].Split("-")[1]) * -1;
+                }
+                catch
+                {
+                    Console.WriteLine("Please use valid formatting");
+                    return;
+                }
             }
-            Console.WriteLine("You are rolling " + usrInput[0] + " dice with " + usrInput[1] + " faces");
-            
+            else
+            {
+                try
+                {
+                    rolled = Convert.ToInt32(usrInput[0]);
+                    faces = Convert.ToInt32(usrInput[1]);
+                    modifier = 0;
+                }
+                catch
+                {
+                    Console.WriteLine("Please use valid formatting");
+                    return;
+                }
+            }
+
             string result = "";
             Random rand = new Random();
             for (int i = 0; i < rolled; i++)
             {
-                result += "Roll " + (i + 1) + ": " + rand.Next(1, faces) + "\n";
+                result += "Roll " + (i + 1) + ": " + (rand.Next(1, faces) + modifier) + " (" + modifier + " modifier)\n";
             }
             Console.WriteLine(result.Substring(0, result.Length - 1));
         }
